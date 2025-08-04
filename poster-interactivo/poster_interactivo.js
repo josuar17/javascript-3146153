780817e72880db143ac691ae18433e2385e9a36b
josuar17 <josuaravila1707@gmail.com>
2025-08-04 16:10:17 -0500

    Add files via upload

---
+++ poster-interactivo/poster_interactivo.js
@@ -0,0 +1,16 @@
+const monedas = document.querySelectorAll(".coin")
+const corazon = document.querySelector(".corazon")
+const contadorMonedas = document.querySelector("#contador-monedas")
+const contadorCorazones = document.querySelectorAll("#contador-corazones")
+
+console.log(monedas, corazon, contadorMonedas, contadorCorazones)
+
+monedas.forEach( function (moneda){
+   
+    moneda.addEventListener( "click" , function () {
+        moneda.classList.add("saltar")
+        contadorMonedas.textContent = parseInt(contadorMonedas.textContent) + 1
+        
+    } )
+
+} )
