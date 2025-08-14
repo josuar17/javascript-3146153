c5a11927d676dd189196939f73e61d1c2a47ad52
josuar17 <josuaravila1707@gmail.com>
2025-08-14 15:33:20 -0500

    audio y video parte 1

---
+++ 4_audio_video_js/app.js
@@ -0,0 +1,9 @@
+let video  = document.querySelector("#video");
+let playVideo = document.querySelector("#play-video");
+let stopVideo = document.querySelector("#stop-video");
+let playStopImg1 = document.querySelector("#play-stop-img1");
+let playStopImg2 = document.querySelector("#play-stop-img2");
+
+console.log(video , playVideo , stopVideo , playStopImg1 , playStopImg2);
+
+
